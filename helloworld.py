3d8a29d876ad9029ef154681752ead416d8e7a97
Jemuel Santos <santosje@grinnell.edu>
2018-07-20 17:37:32 -0500

    asdas

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,94 @@
+import random
+
 print("Hello World!")
+
+
+def reverseString(s):
+    ret = []
+
+    for i in range(reversed(range(0, len(s)))):
+        ret.append(s[i])
+    return "".join(ret)
+
+
+def fizzBuzz(i):
+    for z in range(1, i + 1):
+        if z % 3 == 0 and z % 5 == 0:
+            print("FizzBuzz")
+        elif z % 3 == 0:
+            print("Fizz")
+        elif z % 5 == 0:
+            print("Buzz")
+        else:
+            print(z)
+
+
+def factorial(i):
+    if i == 0:
+        return 1
+
+    return i * factorial(i-1)
+
+
+def linearSearch(A, i):
+    pos = 0
+
+    while pos < len(A):
+        if A[pos] == i:
+            return pos
+        pos += 1
+    return -1
+
+
+array = [1, 5, 2, 3, 7, 8]
+set = {5, 1, 2, 8, 6, 4, 7}
+
+
+print(linearSearch(array, 10))
+
+
+def divBy2():
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+def listLessThan10(A):
+    ret = []
+    for i in range(len(A)):
+        if A[i] < 5:
+            ret.append(A[i])
+
+
+def divisors(n):
+    for i in range(1, n + 1):
+        if (n % i) == 0:
+            print(i)
+
+
+divisors(26)
+
+
+def commons():
+    a = range(1, random.randint(1, 30))
+    b = range(1, random.randint(10, 40))
+    c = []
+
+    for i in range (len(a)):
+        if a[i] in b:
+            if a[i] not in c:
+                c.append(a[i])
+    print(a)
+    print(b)
+    print(c)
+
+
+def palindrome(s):
+    if s[:] == s[::-1]:
+        print ("true")
+    else:
+        print ("false")
+
+palindrome("abcba")
---
+++ learningTensorflow.py
@@ -0,0 +1,72 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Get Data
+fashion_mnist = keras.datasets.fashion_mnist
+
+# Load Data
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.gca().grid(False)
+
+# Build Model
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+# Compile Model
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+# Train Model
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+
+# Grab an image from the test dataset
+img = test_images[0]
+
+# Add the image to a batch where it's the only member.
+img = (np.expand_dims(img,0))
+
+print(img.shape)
+
+predictions = model.predict(img)
+
+print(predictions)
+
+# Plot the first 25 test images, their predicted label, and the true label
+# Color correct predictions in green, incorrect predictions in red
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid('off')
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    predicted_label = np.argmax(predictions[i])
+    true_label = test_labels[i]
+    if predicted_label == true_label:
+      color = 'green'
+    else:
+      color = 'red'
+    plt.xlabel("{} ({})".format(class_names[predicted_label],
+                                  class_names[true_label]),
+                                  color=color)
